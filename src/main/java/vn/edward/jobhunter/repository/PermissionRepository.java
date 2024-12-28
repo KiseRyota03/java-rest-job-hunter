dba7b5dbbe62c06611eb1db698d90894344533fc
KiseRyota03 <tthiep.cv@gmail.com>
2024-12-28 10:35:35 +0700

    feat(permission): CRUD permission

--- src/main/java/vn/edward/jobhunter/repository/PermissionRepository.java
+++ src/main/java/vn/edward/jobhunter/repository/PermissionRepository.java
@@ -1,6 +1,5 @@
 package vn.edward.jobhunter.repository;
 
-import org.springframework.stereotype.Repository;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
@@ -8,6 +7,9 @@ import org.springframework.stereotype.Repository;
 import vn.edward.jobhunter.domain.Permission;
 
 @Repository
-public class PermissionRepository {
+public interface PermissionRepository extends JpaRepository<Permission, Long>,
+    JpaSpecificationExecutor<Permission> {
+  boolean existsByModuleAndApiPathAndMethod(String module, String apiPath, String method);
 
+  List<Permission> findByIdIn(List<Long> id);
 }
