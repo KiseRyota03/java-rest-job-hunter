587f2bceeba39b31c5f760596959edd9841458fb
KiseRyota03 <tthiep.cv@gmail.com>
2024-12-28 11:07:41 +0700

    feat(auht): config token

---
+++ src/main/java/vn/edward/jobhunter/controller/AuthController.java
@@ -0,0 +1,92 @@
+package vn.edward.jobhunter.controller;
+
+import org.apache.catalina.security.SecurityUtil;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import jakarta.validation.Valid;
+import vn.edward.jobhunter.domain.User;
+import vn.edward.jobhunter.domain.request.ReqLoginDTO;
+import vn.edward.jobhunter.domain.response.ResLoginDTO;
+import vn.edward.jobhunter.service.UserService;
+
+@RestController
+@RequestMapping("/api/v1")
+public class AuthController {
+  private final AuthenticationManagerBuilder authenticationManagerBuilder;
+  private final SecurityUtil securityUtil;
+  private final UserService userService;
+  private final PasswordEncoder passwordEncoder;
+
+  @Value("${hoidanit.jwt.refresh-token-validity-in-seconds}")
+  private long refreshTokenExpiration;
+
+  public AuthController(
+      AuthenticationManagerBuilder authenticationManagerBuilder,
+      SecurityUtil securityUtil,
+      UserService userService,
+      PasswordEncoder passwordEncoder) {
+    this.authenticationManagerBuilder = authenticationManagerBuilder;
+    this.securityUtil = securityUtil;
+    this.userService = userService;
+    this.passwordEncoder = passwordEncoder;
+  }
+
+  @PostMapping("/auth/login")
+  public ResponseEntity<ResLoginDTO> login(@Valid @RequestBody ReqLoginDTO loginDto) {
+    // Nạp input gồm username/password vào Security
+    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+        loginDto.getUsername(), loginDto.getPassword());
+
+    // xác thực người dùng => cần viết hàm loadUserByUsername
+    Authentication authentication = authenticationManagerBuilder.getObject()
+        .authenticate(authenticationToken);
+
+    // set thông tin người dùng đăng nhập vào context (có thể sử dụng sau này)
+    SecurityContextHolder.getContext().setAuthentication(authentication);
+
+    ResLoginDTO res = new ResLoginDTO();
+    User currentUserDB = this.userService.handleGetUserByUsername(loginDto.getUsername());
+    if (currentUserDB != null) {
+      ResLoginDTO.UserLogin userLogin = new ResLoginDTO.UserLogin(
+          currentUserDB.getId(),
+          currentUserDB.getEmail(),
+          currentUserDB.getName(),
+          currentUserDB.getRole());
+      res.setUser(userLogin);
+    }
+
+    // create access token
+    String access_token = this.securityUtil.createAccessToken(authentication.getName(), res);
+    res.setAccessToken(access_token);
+
+    // create refresh token
+    String refresh_token = this.securityUtil.createRefreshToken(loginDto.getUsername(), res);
+
+    // update user
+    this.userService.updateUserToken(refresh_token, loginDto.getUsername());
+
+    // set cookies
+    ResponseCookie resCookies = ResponseCookie
+        .from("refresh_token", refresh_token)
+        .httpOnly(true)
+        .secure(true)
+        .path("/")
+        .maxAge(refreshTokenExpiration)
+        .build();
+
+    return ResponseEntity.ok()
+        .header(HttpHeaders.SET_COOKIE, resCookies.toString())
+        .body(res);
+  }
+
+}
---
+++ src/main/java/vn/edward/jobhunter/util/SecurityUtil.java
@@ -0,0 +1,59 @@
+package vn.edward.jobhunter.util;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwsHeader;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+
+import vn.edward.jobhunter.domain.response.ResLoginDTO;
+
+public class SecurityUtil {
+  private final JwtEncoder jwtEncoder;
+
+  public SecurityUtil(JwtEncoder jwtEncoder) {
+    this.jwtEncoder = jwtEncoder;
+  }
+
+  public static final MacAlgorithm JWT_ALGORITHM = MacAlgorithm.HS512;
+
+  @Value("${jwt.base64-secret}")
+  private String jwtKey;
+
+  @Value("${jwt.access-token-validity-in-seconds}")
+  private long accessTokenExpiration;
+
+  @Value("${jwt.refresh-token-validity-in-seconds}")
+  private long refreshTokenExpiration;
+
+  public String createAccessToken(String email, ResLoginDTO dto) {
+    ResLoginDTO.UserInsideToken userToken = new ResLoginDTO.UserInsideToken();
+    userToken.setId(dto.getUser().getId());
+    userToken.setEmail(dto.getUser().getEmail());
+    userToken.setName(dto.getUser().getName());
+
+    Instant now = Instant.now();
+    Instant validity = now.plus(this.accessTokenExpiration, ChronoUnit.SECONDS);
+
+    List<String> listAuthority = new ArrayList<String>();
+
+    JwtClaimsSet claims = JwtClaimsSet.builder()
+        .issuedAt(now)
+        .expiresAt(validity)
+        .subject(email)
+        .claim("user", userToken)
+        .claim("permission", listAuthority)
+        .build();
+
+    JwsHeader jwtHeader = JwsHeader.with(JWT_ALGORITHM).build();
+    return this.jwtEncoder.encode(JwtEncoderParameters.from(jwtHeader, claims)).getTokenValue();
+
+  }
+
+}
